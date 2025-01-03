fb8b867cf82f0492bde6b4bf4655dd57f1f9cb8f
martinolee <soohan.m.lee@gmail.com>
2025-01-03 17:11:09 +0900

    Add default project

---
+++ Slate/Slate/SlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlateApp.swift
+//  Slate
+//
+//  Created by Soohan Lee on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SlateApp: App {
+  var body: some Scene {
+    WindowGroup {
+      EmptyView()
+    }
+  }
+}
