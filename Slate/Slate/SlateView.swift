83fee43447e3134549f104cc9df33d2d25a5477c
martinolee <soohan.m.lee@gmail.com>
2025-01-03 21:01:06 +0900

    Implement SlateView

---
+++ Slate/Slate/SlateView.swift
@@ -0,0 +1,64 @@
+//
+//  SlateView.swift
+//  Slate
+//
+//  Created by Soohan Lee on 1/3/25.
+//
+
+import SwiftUI
+import SwiftData
+import AVFoundation
+
+struct SlateView: View {
+  @AppStorage("title") var title = ""
+  @AppStorage("subtitle") var subtitle = ""
+  @State private var date = ""
+  private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+  private let dateFomatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+    return dateFormatter
+  }()
+  @State private var backgorundColor = Color.white
+  
+  var body: some View {
+    ScrollView {
+      VStack(alignment: .center, spacing: .zero) {
+        Button("Action!") {
+          AudioServicesPlaySystemSound(1054)
+          Task {
+            await flashBackground()
+          }
+        }
+        .font(.system(size: 100, weight: .bold))
+        .padding()
+        .overlay(RoundedRectangle(cornerRadius: 25).stroke(Color.blue, lineWidth: 10))
+        TextField("Title", text: $title)
+          .font(.system(size: 60, weight: .bold))
+          .multilineTextAlignment(.center)
+        TextField("Subtitle", text: $subtitle)
+          .font(.system(size: 60, weight: .bold))
+          .multilineTextAlignment(.center)
+        Text(date)
+          .font(.system(size: 60, weight: .bold))
+          .onReceive(timer, perform: { _ in
+            date = dateFomatter.string(from: Date())
+          })
+      }
+    }
+    .background(backgorundColor)
+  }
+}
+
+extension SlateView {
+
+  private func flashBackground() async {
+    backgorundColor = Color.red
+    try? await Task.sleep(for: .seconds(0.2))
+    backgorundColor = Color.white
+  }
+}
+
+#Preview {
+  SlateView()
+}
